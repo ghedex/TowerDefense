c6da43320bd657e6b5af51bf82f7df12a5eec34c
MiStchec <michael.stecklein@gmx.de>
2020-10-28 22:18:50 +0100

    Added image rescale on window size

---
+++ desktop/src/com/jem/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.jem.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jem.game.TowerDefenseGame;
+import levels.levelGenerator;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 720;
+		config.width = 1280;
+		new LwjglApplication(new levelGenerator("game_background_1.png"), config);
+	}
+}
