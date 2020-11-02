6263430856cbc3cc892652ab1fde37df9004d342
MiStchec <michael.stecklein@gmx.de>
2020-11-02 20:40:33 +0100

    modified entity file

--- core/src/enemy/Entity.java
+++ core/src/enemy/Entity.java
@@ -4,10 +4,12 @@ public class Entity {
 
     public final String entityFile;
     public int damage = 0;
+    public int speed;
 
-    public Entity(String entityFile, int damage){
+    public Entity(String entityFile, int damage, int speed){
         this.entityFile = entityFile;
         this.damage = damage;
+        this.speed = speed;
     }
 
 }
