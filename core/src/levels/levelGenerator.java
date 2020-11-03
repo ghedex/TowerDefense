c4924053303c2a4763126b37f168184212d936c2
MiStchec <michael.stecklein@gmx.de>
2020-11-03 20:47:39 +0100

    created monster and some files

--- core/src/levels/levelGenerator.java
+++ core/src/levels/levelGenerator.java
@@ -6,28 +6,34 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import enemy.Scorpion.Scorpion;
+
+
+
 public class levelGenerator extends ApplicationAdapter {
     private final String level;
 
     SpriteBatch batch;
     Texture img;
+    Texture scorp;
     public levelGenerator(String level){
         this.level = level;
     }
 
-
+    Scorpion enemy = new Scorpion();
     @Override
     public void create () {
         batch = new SpriteBatch();
         img = new Texture(level);
+        scorp = new Texture(enemy.entityFile);
+
     }
     @Override
     public void render () {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         //last both params allow to scale image to window size
         batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(scorp, 0,0);
         batch.end();
     }
 
@@ -35,5 +41,6 @@ public class levelGenerator extends ApplicationAdapter {
     public void dispose () {
         batch.dispose();
         img.dispose();
+        scorp.dispose();
     }
 }
