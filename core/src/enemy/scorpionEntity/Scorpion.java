ae1998788ed96f43c462117a62eed3db50fa8e85
MiStchec <michael.stecklein@gmx.de>
2021-04-29 20:27:23 +0200

    setup wave

--- core/src/enemy/scorpionEntity/Scorpion.java
+++ core/src/enemy/scorpionEntity/Scorpion.java
@@ -10,32 +10,19 @@ import levels.LevelOne;
 import levels.PathfindingEnemy;
 
 
-public class Scorpion extends Sprite {
+public class Scorpion extends Entity {
 
-    Vector2 first;
-
-    Vector2 second;
 
     public Scorpion() {
 
-        super( new Texture( Gdx.files.internal( "core/assets/assetsPack/scorpions/scorpionRunning/1_enemies_1_run_000.png" ) ) );
+        super(90, 90, 100, "core/assets/assetsPack/scorpions/scorpionRunning/scorpionPack.atlas");
 
         super.setSize(90, 90);
         super.setPosition(LevelOne.levelOnePath().first().x, LevelOne.levelOnePath().first().y);
         //super.setPosition(posX, posY);
 
-
     }
 
-    public void move(){
-        this.translateX(1);
-    }
-
-
-    public void draw(SpriteBatch batch, float x, float y){
-        this.draw(batch, x, y);
-
-    }
 
     //TO DO: Skorpion muss schon animiert sein, damit ich nur den Skorpion callen muss
 
--- core/src/levels/levelGenerator.java
+++ core/src/levels/levelGenerator.java
@@ -7,13 +7,8 @@ import com.badlogic.gdx.Gdx;
 
 import com.badlogic.gdx.Screen;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.*;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Bezier;
-import com.badlogic.gdx.math.CatmullRomSpline;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.*;
+
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -28,10 +23,8 @@ import enemy.scorpionEntity.Scorpion;
 import levels.menu.testActor;
 import levels.menu.testMainMenu;
 
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.logging.Level;
+
 
 
 public class levelGenerator implements Screen {
@@ -45,26 +38,15 @@ public class levelGenerator implements Screen {
     PathfindingEnemy scorpionEnemy;
     PathfindingEnemy wizardEnemy;
     Scorpion scorpion;
-    Scorpion scorpion2;
     private float timePassed;
     private boolean isPaused;
     private String pauseButton = "menuAssets/mainMenuAssets/buttonAssets/pauseButton.png";
     private Skin skin;
-    private LinkedList<Scorpion> scorpionLinkedList;
+    private LinkedList<PathfindingEnemy> scorpionLinkedList;
     Wizard wizard;
     Sprite a;
     private float timeBetweenEnemySpawns = 3f;
     private float enemySpawnTimer;
-    ShapeRenderer shapeRenderer;
-    float speed = 0.15f;
-    float current = 0;
-    int k = 1000; //increase k for more fidelity to the spline
-    Vector2[] points = new Vector2[k];
-    private ShapeRenderer sr;
-    int waypoint = 1;
-    private Vector2 velocity = new Vector2();
-    private float tolerance = 3;
-    private CatmullRomSpline<Vector2> scorpionPath;
     public levelGenerator(final TowerDefense game) {
         this.game = game;
     }
@@ -126,36 +108,7 @@ public class levelGenerator implements Screen {
         scorpion = new Scorpion();
 
         wizard = new Wizard();
-        shapeRenderer = new ShapeRenderer();
 
-
-        scorpionPath = new CatmullRomSpline<Vector2>(new Vector2[] {
-                new Vector2(0, -150),
-                new Vector2(10, 150),
-                new Vector2(250, 150),
-                new Vector2(360, 175),
-                new Vector2(410, 225),
-                new Vector2(440, 300),
-                new Vector2(500, 360),
-//                new Vector2(525, 375),
-                new Vector2(625, 350),
-                new Vector2(700, 325),
-                new Vector2(750, 325),
-                new Vector2(800, 375),
-                new Vector2(850, 500),
-                new Vector2(900, 550),
-                new Vector2(1000, 550),
-                new Vector2(1100, 550),
-                new Vector2(1200, 500),
-                new Vector2(1400, 500),
-        }, false);
-
-
-        for(int i = 0; i < k; ++i)
-        {
-            points[i] = new Vector2();
-            scorpionPath.valueAt(points[i], ((float)i)/((float)k-1));
-        }
     }
 
     @Override
@@ -166,76 +119,26 @@ public class levelGenerator implements Screen {
         if (!isPaused){
             spawnEnemyScorpions(Gdx.graphics.getDeltaTime());
         }
-
-        current += Gdx.graphics.getDeltaTime() * speed;
-        if(current >= 1)
-            current -= 1;
-        float place = current * k;
-
-        //scorpion.setFirst(points[(int)place]);
-        if(((int)place+1) < k){
-            //second = points[(int)place+1];
-//            scorpion.setSecond(points[(int)place+1]);
-        }
-        else{
-            //second = points[0];
-//            scorpion.setSecond(points[0]);//or finish, in case it does not loop.
-        }
-        float t = place - ((int)place); //the decimal part of place
-
         stage.act(Gdx.graphics.getDeltaTime());
-
-
 
         stage.draw();
 
-        /*
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        for(int i = 0; i < k-1; ++i)
-        {
-            shapeRenderer.line(points[i], points[i+1]);
-        }
-        shapeRenderer.end();
-
-         */
-
-        /*
-        Iterator<Scorpion> scorpionIterator = scorpionLinkedList.iterator();
-        while(scorpionIterator.hasNext()){
-            Scorpion scorpion = scorpionIterator.next();
-
-            scorpion.draw(batch);
-            //scorpion.setPosition(scorpion.getFirst().x + (scorpion.getSecond().x - scorpion.getFirst().x) * t, scorpion.getFirst().y + (scorpion.getSecond().y - scorpion.getFirst().y) * t);
-
-        }
-
-
-         */
-
-
-
-        for(Scorpion s: scorpionLinkedList){
-
-            //s.draw(batch, first.x + (second.x - first.x) * t, first.y + (second.y - first.y) * t);
-            //batch.draw(s, scorpion.getFirst().x + (scorpion.getSecond().x - scorpion.getFirst().x) * t, scorpion.getFirst().y + (scorpion.getSecond().y - scorpion.getFirst().y) * t);
-
-            s.setPosition(-150, 150);
-
-            //s.setPosition(s.getFirst().x + (s.getSecond().x - s.getFirst().x) * t, s.getFirst().y + (s.getSecond().y - s.getFirst().y) * t);
-            s.translate(1,1);
-
-        }
-        //System.out.println(scorpionLinkedList.toString());
-
+        makeEnemiesMove();
 
         batch.end();
+    }
+
+    public void makeEnemiesMove(){
+        for(PathfindingEnemy s: scorpionLinkedList){
+            s.update(batch, LevelOne.levelOnePath());
+        }
     }
 
 
     public void spawnEnemyScorpions(float deltaTime){
         enemySpawnTimer += deltaTime;
         if(enemySpawnTimer > timeBetweenEnemySpawns){
-            scorpionLinkedList.add(new Scorpion());
+            scorpionLinkedList.add(new PathfindingEnemy(scorpion.idleFrame()));
             enemySpawnTimer -= timeBetweenEnemySpawns;
         }
     }
@@ -263,7 +166,7 @@ public class levelGenerator implements Screen {
     @Override
     public void dispose(){
         batch.dispose();
-        scorpion.getTexture().dispose();
+        scorpion.getStage().dispose();
         scorpionEnemy.getTexture().dispose();
         wizard.getTexture().dispose();
         wizardEnemy.getTexture().dispose();
